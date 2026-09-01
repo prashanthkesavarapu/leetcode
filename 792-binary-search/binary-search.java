615ced1cbe565df092ff74fb320edca5237648e3
prashanthkesavarapu <prashanthkesavarapu@gmail.com>
2026-09-01 13:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (94.63%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int search(int[] nums, int target) {
+        int index = Arrays.binarySearch(nums, target);
+        if (index>=0){
+        return index; }
+        else{
+            return -1;
+        }
+        
+    }
+}
